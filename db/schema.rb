34c7e07bef0e5393cc5caa67505f64b07b278933
mbarrins <maija.barrins@gmail.com>
2019-05-24 22:00:56 +0100

    ran db:migrate to update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190524202550) do
+ActiveRecord::Schema.define(version: 20190524204508) do
+
+  create_table "user_events", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "event_id"
+    t.string  "status"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
