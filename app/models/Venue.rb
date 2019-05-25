ce84dfc44d5605c683e94d184693ae910e4449c2
mbarrins <maija.barrins@gmail.com>
2019-05-26 00:21:58 +0100

    corrected syntax on changes made

--- app/models/Venue.rb
+++ app/models/Venue.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   has_many :events
   has_many :user_events, through: :events
-  has_may :users, through: user_events
-  has_many: reviews, through: user_events
+  has_many :users, through: :user_events
+  has_many :reviews, through: :user_events
 end
