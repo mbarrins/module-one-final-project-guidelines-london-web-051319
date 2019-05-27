d84d9e23ae44a3d24a6730e6182410b251737cea
mbarrins <maija.barrins@gmail.com>
2019-05-27 22:39:25 +0100

    corrected event_dates migration

--- db/migrate/20190527195746_create_event_dates.rb
+++ db/migrate/20190527195746_create_event_dates.rb
@@ -1,7 +1,7 @@
 class CreateEventDates < ActiveRecord::Migration[5.0]
   def change
     def change
-      create_table :events do |t|
+      create_table :event_dates do |t|
         t.string :tm_event_date_id
         t.string :event_date_name
         t.string :url
