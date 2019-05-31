290bbe2d122c09b1eb530ee0ee2382a32d36fc50
mbarrins <maija.barrins@gmail.com>
2019-05-31 13:34:29 +0100

    required config files

--- config/environment.rb
+++ config/environment.rb
@@ -6,6 +6,7 @@ ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'db/develo
 ActiveRecord::Base.logger = nil
 require_all 'lib'
 require_all 'app'
+require_all 'config'
 
 #APIKEY = ""  # Please enter Ticketmaster API key here
 EVENTSURL = "https://app.ticketmaster.com/discovery/v2/events?"
