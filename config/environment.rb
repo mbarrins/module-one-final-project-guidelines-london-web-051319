cee6406d5386673d2c1f086ef13db59a9731dae4
mbarrins <maija.barrins@gmail.com>
2019-05-31 13:32:12 +0100

    cleared actual api key from environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -7,7 +7,7 @@ ActiveRecord::Base.logger = nil
 require_all 'lib'
 require_all 'app'
 
-APIKEY = "sqj3GomEiDTyMWkLzNhfrH0r62ZC82EA"
+#APIKEY = ""  # Please enter Ticketmaster API key here
 EVENTSURL = "https://app.ticketmaster.com/discovery/v2/events?"
 CLASSURL = "https://app.ticketmaster.com/discovery/v2/classifications?"
 PER_PAGE = 20
